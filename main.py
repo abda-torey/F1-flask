4877f4c6512b955d9707b5a237a7cdc4c28d7853
abda-torey <abdatorey@gmail.com>
2023-03-12 10:41:27 +0000

    added filter options for drivers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import google.oauth2.id_token
 from google.auth.transport import requests
 from google.cloud import datastore
@@ -26,9 +26,15 @@ def create_team(name, year_founded, total_p_pos, total_race_w, total_c_titles, p
 def retrieveTeams():
     query = datastore_client.query(kind='Teams')
     teams = query.fetch()
-    
 
     return teams
+
+
+def retrieveDrivers():
+    query = datastore_client.query(kind='Drivers')
+    drivers = query.fetch()
+
+    return drivers
 
 
 def create_driver(name, age, pole_positions, total_race_wins, total_points_scored, total_world_titles, total_fastest_laps, team):
@@ -49,6 +55,23 @@ def create_driver(name, age, pole_positions, total_race_wins, total_points_score
     datastore_client.put(entity)
 
 
+@app.route('/filter_drivers', methods=['POST'])
+def filterDriver():
+    result = None
+
+    team = request.form['team']
+    wins = request.form['wins']
+    query = datastore_client.query(kind='Drivers')
+    if team:
+        query.add_filter('team', '=', team)
+    if wins:
+        query.add_filter('total_race_wins', '>=', wins)
+
+    drivers = query.fetch()
+
+    return render_template('driver.html', drivers=drivers)
+
+
 @app.route('/teams')
 def teams():
     teamNames = retrieveTeams()
@@ -59,65 +82,47 @@ def teams():
 
 @app.route('/add_team', methods=['POST'])
 def addTeam():
-    id_token = request.cookies.get("token")
-    claims = None
-    error_message = None
-
-    if id_token:
-        try:
-            claims = google.oauth2.id_token.verify_firebase_token(
-                id_token, firebase_request_adapter)
-            name = request.form['name']
-            year_founded = request.form['year_founded']
-            total_p_pos = request.form['total_p_pos']
-            total_race_w = request.form['total_race_w']
-            total_c_titles = request.form['total_c_titles']
-            prevSeason_pos = request.form['prevSeason_pos']
-            create_team(name, year_founded, total_p_pos,
-                        total_race_w, total_c_titles, prevSeason_pos)
-
-        except ValueError as exc:
-            error_message = str(exc)
-
-    return render_template('teams.html', user=claims, error_message=error_message)
+    
+    name = request.form['name']
+    year_founded = request.form['year_founded']
+    total_p_pos = request.form['total_p_pos']
+    total_race_w = request.form['total_race_w']
+    total_c_titles = request.form['total_c_titles']
+    prevSeason_pos = request.form['prevSeason_pos']
+    create_team(name, year_founded, total_p_pos,
+                total_race_w, total_c_titles, prevSeason_pos)
+    return redirect(url_for('teams'))
+    return render_template('teams.html')
 
 
 @app.route('/drivers')
-
 def drivers():
-    teamNames = retrieveTeams()
+    teamNames = list(retrieveTeams())
+    drivers = retrieveDrivers()
+    
 
-    return render_template('drivers.html',teamNames = teamNames)
+    return render_template('driver.html', teamNames=teamNames, drivers=drivers)
 
 
-@app.route('/add_driver',methods=['POST'])
+@app.route('/add_driver', methods=['POST'])
 def addDriver():
-    id_token = request.cookies.get("token")
-    claims = None
+
     error_message = None
     teamNames = None
+    name = request.form['firstName']
+    age = request.form['age']
+    pole_positions = request.form['pole_positions']
+    total_race_wins = request.form['total_race_wins']
+    total_points_scored = request.form['total_points_scored']
+    total_world_titles = request.form['total_world_titles']
+    total_fastest_laps = request.form['total_fastest_laps']
+    team = int(request.form['team'])
+    create_driver(name, age,
+                  pole_positions, total_race_wins, total_points_scored, total_world_titles, total_fastest_laps, team)
+    teamNames = retrieveTeams()
+    return redirect(url_for('drivers'))
 
-    if id_token:
-        try:
-            claims = google.oauth2.id_token.verify_firebase_token(
-                id_token, firebase_request_adapter)
-            name = request.form['firstName']
-            age = request.form['age']
-            pole_positions = request.form['pole_positions']
-            total_race_wins = request.form['total_race_wins']
-            total_points_scored = request.form['total_points_scored']
-            total_world_titles = request.form['total_world_titles']
-            total_fastest_laps = request.form['total_fastest_laps']
-            team = request.form['team']
-            create_driver(name, age,
-                          pole_positions, total_race_wins, total_points_scored, total_world_titles, total_fastest_laps, team)
-            teamNames = retrieveTeams()
-            return redirect(url_for('drivers'))
-        except ValueError as exc:
-            error_message = str(exc)
-
-
-    return render_template('drivers.html',teamNames = teamNames)
+    return render_template('driver.html', teamNames=teamNames)
 
 
 @app.route('/')
@@ -135,7 +140,7 @@ def root():
 
     print("claims:", claims)
 
-    return render_template('index.html', user=claims, error_message=error_message)
+    return render_template('base.html', user=claims, error_message=error_message)
 
 
 if __name__ == '__main__':
